e1eaa5731f7fd0c329d58939f95dbfe15a9fc6b2
Justin <JEStaubach@users.noreply.github.com>
2023-07-04 16:20:13 -0400

    add tooling

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,8 @@ export default defineConfig({
   })],
   test: {
     coverage: {
-      provider: 'istanbul'
+      provider: 'istanbul',
+      reporter: [`text`, `json`, `html`, `lcov`]
     }
   },
 });
