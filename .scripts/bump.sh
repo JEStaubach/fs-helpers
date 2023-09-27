75bc52c452c3b7b9af9db31d79252f75e3e7a4e7
Justin <JEStaubach@users.noreply.github.com>
2023-09-26 23:22:01 -0400

    output the projectversion as a sanity check

--- .scripts/bump.sh
+++ .scripts/bump.sh
@@ -2,4 +2,5 @@
 npm version $1 && \
 grep version package.json | cut -f 4 -d '"' | xargs -I OutputFromGrep sed -Ei 's/(sonar.projectVersion=).*$/\1OutputFromGrep/g' sonar-project.properties && \
 git add sonar-project.properties && \
-git commit -m "Bump SonarCloud version to match package.json"
+git commit -m "Bump SonarCloud version to match package.json" && \
+grep sonar-project.properties -e "sonar.projectVersion="
