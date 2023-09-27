1fbe742a45428243a31c6b65a0546d31b8ea7eba
Justin <JEStaubach@users.noreply.github.com>
2023-09-26 23:17:12 -0400

    chain script commands so that if one fails the following do as well, could help with bad version names.

--- .scripts/bump.sh
+++ .scripts/bump.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm version $1
-grep version package.json | cut -f 4 -d '"' | xargs -I OutputFromGrep sed -Ei 's/(sonar.projectVersion=).*$/\1OutputFromGrep/g' sonar-project.properties
-git add sonar-project.properties
+npm version $1 && \
+grep version package.json | cut -f 4 -d '"' | xargs -I OutputFromGrep sed -Ei 's/(sonar.projectVersion=).*$/\1OutputFromGrep/g' sonar-project.properties && \
+git add sonar-project.properties && \
 git commit -m "Bump SonarCloud version to match package.json"
